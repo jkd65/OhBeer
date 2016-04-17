c2ef6f19922227ffa475dccc066fa7e82189ec05
Keevan Dance <keevandance@nau.edu>
2016-04-16 19:43:08 -0700

    added longitude and latitude

--- src/app/mobile/templates/authenticated/index.js
+++ src/app/mobile/templates/authenticated/index.js
@@ -1,7 +1,20 @@
-if (Meteor.isCordova){
+  if (Meteor.isCordova){
 
-  Template.indexMobile.onCreated( () => {
-    
-  });
+  Template.indexMobile.onRendered( () => {
+  })
+
+  Template.indexMobile.helpers({
+    getLocation() {
+      let location = Session.get('currentLocation')
+      return location
+    }
+  })
+
+  Template.indexMobile.events({
+    'click .location' (event){
+      Session.set('currentLocation', Geolocation.latLng());
+    }
+  })
+
 
 }
